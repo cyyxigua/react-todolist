f12649629f2db9cff95c8250d9a97434a02c0ed6
yychen <yychen@utstar.com>
2020-09-02 17:38:13 +0800

    feat: todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem'
+import './style.css'
 
 class TodoList extends Component {
 
@@ -8,28 +10,64 @@ class TodoList extends Component {
       inputValue: '111',
       list: []
     }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleItemDelete = this.handleItemDelete.bind(this)
   }
 
   render() {
     return (
       <Fragment>
         <div>
-          <input 
+          <label htmlFor="insertArea">输入内容</label>
+          <input
+            id="insertArea"
             value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
           />
-          <button>提交</button>
+          <button onClick={this.handleBtnClick}>提交</button>
         </div>
         <ul>
-          <li>学英语</li>
+          { this.getTodoItem() }
         </ul>
       </Fragment>
     )
   }
 
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem 
+          key={index}
+          content={item} 
+          index={index} 
+          deleteItem={this.handleItemDelete}
+        />
+      )
+    })
+  }
+
   handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
+    const value = e.target.value
+    this.setState(() => ({
+      inputValue: value
+    }))
+  }
+
+  handleBtnClick() {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, prevState.inputValue],
+        inputValue: ''
+      }
+    })
+  }
+
+  handleItemDelete(index) {
+    this.setState((prevState) => {
+      const list = [...prevState.list]
+      list.splice(index, 1)
+      return {list}
     })
   }
 }
